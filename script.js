bc381f0d2449b8a71b97c80f1bc8830e9c10f24c
reworldcreative <reworldcreative@gmail.com>
2023-03-09 10:23:49 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  $(".top-slider").slick({
+    arrows: false,
+    slidesToShow: 3,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1580,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  $(".persons-slider").slick({
+    arrows: false,
+    slidesToShow: 3,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1580,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+
+      {
+        breakpoint: 710,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
