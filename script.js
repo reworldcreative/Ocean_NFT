20c0d0cca56b2bb54deb04dcd679dac4802f54f6
reworldcreative <reworldcreative@gmail.com>
2023-03-18 16:29:24 +0200

    add preloader

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.onload = function () {
+  document.body.classList.add("loaded_hiding");
+  window.setTimeout(function () {
+    document.body.classList.add("loaded");
+    document.body.classList.remove("loaded_hiding");
+  }, 300);
+};
+
 $(document).ready(function () {
   $(".top-slider").slick({
     arrows: false,
